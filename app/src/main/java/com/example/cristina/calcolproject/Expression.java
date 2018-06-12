88bffa24030cebf67ec3d871855cd805bc0f981b
cristinaochner <37936458+cristinaochner@users.noreply.github.com>
2018-06-12 19:03:23 +0200

    Modifica espressione

--- app/src/main/java/com/example/cristina/calcolproject/Expression.java
+++ app/src/main/java/com/example/cristina/calcolproject/Expression.java
@@ -15,42 +15,7 @@ public abstract class Expression {
             return false;
         }
     }
-
-    public static Expression polacca1(ArrayList<String> elementi) {
-        Stack<Expression> stack = new Stack<>();
-
-        for (String s : elementi) {
-            if (isNumber(s)) {
-                stack.add(new Number(Double.parseDouble(s)));
-            } else if (s.equals("+")) {
-                Expression e1 = stack.pop();
-                Expression e2 = stack.pop();
-                stack.push(new Add(e2, e1));
-            } else if (s.equals("*")) {
-                Expression e1 = stack.pop();
-                Expression e2 = stack.pop();
-                stack.push(new Mul(e2, e1));
-            } else if (s.equals("-")) {
-                Expression e1 = stack.pop();
-                Expression e2 = stack.pop();
-                stack.push(new Sub(e2, e1));
-            } else if (s.equals("/")) {
-                Expression e1 = stack.pop();
-                Expression e2 = stack.pop();
-                stack.push(new Div(e2, e1));
-            }
-        }
-        return stack.pop();
-    }
-
-    public static Expression polacca2(ArrayList<String> elementi) {
-        ArrayList<String> elementiInversa = new ArrayList<>();
-        for (String s : elementi) {
-            elementiInversa.add(0, s);
-        }
-        return  polacca1(elementiInversa);
-    }
-
+    
     public static Expression algebraic(ArrayList<String> elementi) {
         if (elementi.size() == 1) {
             return new Number(Double.parseDouble(elementi.get(0)));
@@ -77,6 +42,41 @@ public abstract class Expression {
             ArrayList<String> right = new ArrayList<String>(elementi.subList(index + 1, elementi.size()));
             return new Div(algebraic(left), algebraic(right));
         }
+    }
+    
+    public static Expression polacca1(ArrayList<String> elementi) {
+        Stack<Expression> stack = new Stack<>();
+
+        for (String s : elementi) {
+            if (isNumber(s)) {
+                stack.add(new Number(Double.parseDouble(s)));
+            } else if (s.equals("+")) {
+                Expression e1 = stack.pop();
+                Expression e2 = stack.pop();
+                stack.push(new Add(e2, e1));
+            } else if (s.equals("*")) {
+                Expression e1 = stack.pop();
+                Expression e2 = stack.pop();
+                stack.push(new Mul(e2, e1));
+            } else if (s.equals("-")) {
+                Expression e1 = stack.pop();
+                Expression e2 = stack.pop();
+                stack.push(new Sub(e2, e1));
+            } else if (s.equals("/")) {
+                Expression e1 = stack.pop();
+                Expression e2 = stack.pop();
+                stack.push(new Div(e2, e1));
+            }
+        }
+        return stack.pop();
+    }
+    
+    public static Expression polacca2(ArrayList<String> elementi) {
+        ArrayList<String> elementiInversa = new ArrayList<>();
+        for (String s : elementi) {
+            elementiInversa.add(0, s);
+        }
+        return  polacca1(elementiInversa);
     }
 }
 
